773488d5a4193ee85b29ad759ac686cbf0fdc37b
Eric <eric.m2hk@gmail.com>
2020-08-09 17:55:01 +0800

    using reuse function to reduce code

--- navigation-nation/script.js
+++ navigation-nation/script.js
@@ -5,6 +5,17 @@ const nav2 = document.getElementById("nav-2");
 const nav3 = document.getElementById("nav-3");
 const nav4 = document.getElementById("nav-4");
 const nav5 = document.getElementById("nav-5");
+const navItems = [nav1, nav2, nav3, nav4, nav5];
+
+// Control Navigation Animation
+function navAnimation(direction1, direction2) {
+  navItems.forEach((nav, i) => {
+    nav.classList.replace(
+      `slide-${direction1}-${i + 1}`,
+      `slide-${direction2}-${i + 1}`
+    );
+  });
+}
 
 function toggleNav() {
   // Toggle: Menu Bars Open/Close
@@ -16,20 +27,12 @@ function toggleNav() {
     // Animate In - Overlay
     overlay.classList.replace("overlay-slide-left", "overlay-slide-right");
     // Animate In - Nav Items
-    nav1.classList.replace("slide-out-1", "slide-in-1");
-    nav2.classList.replace("slide-out-2", "slide-in-2");
-    nav3.classList.replace("slide-out-3", "slide-in-3");
-    nav4.classList.replace("slide-out-4", "slide-in-4");
-    nav5.classList.replace("slide-out-5", "slide-in-5");
+    navAnimation("out", "in");
   } else {
     // Animate Out - Overlay
     overlay.classList.replace("overlay-slide-right", "overlay-slide-left");
     // Animate Out - Nav Items
-    nav1.classList.replace("slide-in-1", "slide-out-1");
-    nav2.classList.replace("slide-in-2", "slide-out-2");
-    nav3.classList.replace("slide-in-3", "slide-out-3");
-    nav4.classList.replace("slide-in-4", "slide-out-4");
-    nav5.classList.replace("slide-in-5", "slide-out-5");
+    navAnimation("in", "out");
   }
 }
 
