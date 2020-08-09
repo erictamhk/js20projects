cc363ee0ad74c3f873d90d133d3e21a80610b9b6
Eric <eric.m2hk@gmail.com>
2020-08-09 17:50:34 +0800

    animate the overlay and nav items

--- navigation-nation/script.js
+++ navigation-nation/script.js
@@ -14,12 +14,22 @@ function toggleNav() {
   overlay.classList.toggle("overlay-active");
   if (overlay.classList.contains("overlay-active")) {
     // Animate In - Overlay
-    overlay.classList.remove("overlay-slide-left");
-    overlay.classList.add("overlay-slide-right");
+    overlay.classList.replace("overlay-slide-left", "overlay-slide-right");
+    // Animate In - Nav Items
+    nav1.classList.replace("slide-out-1", "slide-in-1");
+    nav2.classList.replace("slide-out-2", "slide-in-2");
+    nav3.classList.replace("slide-out-3", "slide-in-3");
+    nav4.classList.replace("slide-out-4", "slide-in-4");
+    nav5.classList.replace("slide-out-5", "slide-in-5");
   } else {
     // Animate Out - Overlay
-    overlay.classList.remove("overlay-slide-right");
-    overlay.classList.add("overlay-slide-left");
+    overlay.classList.replace("overlay-slide-right", "overlay-slide-left");
+    // Animate Out - Nav Items
+    nav1.classList.replace("slide-in-1", "slide-out-1");
+    nav2.classList.replace("slide-in-2", "slide-out-2");
+    nav3.classList.replace("slide-in-3", "slide-out-3");
+    nav4.classList.replace("slide-in-4", "slide-out-4");
+    nav5.classList.replace("slide-in-5", "slide-out-5");
   }
 }
 
