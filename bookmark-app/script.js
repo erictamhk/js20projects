0a82f9c9074c29a97aeaf9d485facc25992ce0f1
Eric <eric.m2hk@gmail.com>
2020-08-10 17:43:48 +0800

    validate the input

--- bookmark-app/script.js
+++ bookmark-app/script.js
@@ -22,3 +22,35 @@ modalClose.addEventListener("click", () => hideShowModal("hide"));
 window.addEventListener("click", (e) =>
   e.target === modal ? hideShowModal("hide") : false
 );
+
+// Validate Form
+function validate(nameValue, urlValue) {
+  const expression = /(https)?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/g;
+  const regex = new RegExp(expression);
+  if (!nameValue || !urlValue) {
+    alert("Please submit values for both fields.");
+    return false;
+  }
+  if (!urlValue.match(regex)) {
+    alert("Please provide a valid web address.");
+    return false;
+  }
+  // Valid
+  return true;
+}
+
+// Handle Data from Form
+function storeBookmark(e) {
+  e.preventDefault();
+  const nameValue = websiteNameEl.value;
+  let urlValue = websiteUrlEl.value;
+  if (!urlValue.includes("http://", "https://")) {
+    urlValue = `https://${urlValue}`;
+  }
+  if (!validate(nameValue, urlValue)) {
+    return false;
+  }
+}
+
+// Event Listeners
+bookmarkForm.addEventListener("submit", storeBookmark);
