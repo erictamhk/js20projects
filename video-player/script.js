34e86bd4815bce35fedaf0e596e952cd675a8653
Eric <eric.m2hk@gmail.com>
2020-09-08 17:40:42 +0800

    play and pause button

--- video-player/script.js
+++ video-player/script.js
@@ -1,4 +1,33 @@
+const video = document.getElementById("video");
+const progressRange = document.getElementById("progress-range");
+const progressBar = document.getElementById("progress-bar");
+const playBtn = document.getElementById("play-btn");
+const volumeIcon = document.getElementById("volume-icon");
+const volumeRange = document.getElementById("volume-range");
+const volumeBar = document.getElementById("volume-bar");
+const currentTime = document.getElementById("time-elapsed");
+const duration = document.getElementById("time-duration");
+const fullscreenBtn = document.getElementById("fullscreen");
+
 // Play & Pause ----------------------------------- //
+function showPlayIcon() {
+  playBtn.classList.replace("fa-pause", "fa-play");
+  playBtn.setAttribute("title", "Play");
+}
+
+function togglePlay() {
+  if (video.paused) {
+    video.play();
+    playBtn.classList.replace("fa-play", "fa-pause");
+    playBtn.setAttribute("title", "Pause");
+  } else {
+    video.pause();
+    showPlayIcon();
+  }
+}
+
+// On Video End, show play button icon
+video.addEventListener("ended", showPlayIcon);
 
 // Progress Bar ---------------------------------- //
 
@@ -7,3 +36,7 @@
 // Change Playback Speed -------------------- //
 
 // Fullscreen ------------------------------- //
+
+// Event Listeners
+playBtn.addEventListener("click", togglePlay);
+video.addEventListener("click", togglePlay);
